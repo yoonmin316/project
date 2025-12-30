abd0d01c811e0f18ed297d82abbd6a310231cde9
yoonmin316 <43337144+yoonmin316@users.noreply.github.com>
2025-12-30 13:07:21 +0000

    adding Javascript

---
+++ helloWorld.js
@@ -0,0 +1,11 @@
+var a = {
+    array: [1, 2, 3, 4, 5, 6],
+    func: function(){
+        console.log("Hello, World!");
+    }
+};
+
+a.func();
+for(var i = 0; i < a.array.length; i++){
+    console.log("54.9."+(a.array[i]*4+100)+".254");
+}
